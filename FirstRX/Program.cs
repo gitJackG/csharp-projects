dfc2a975b879f9d6b5e3ca4fed069fc0d0dbf976
Jack Griffiths Rico <j.griffithsrico@gmail.com>
2026-02-16 16:53:56 +0000

    understanding observer pattern in RX.NET

--- FirstRX/Program.cs
+++ FirstRX/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
 
 //void Countdown(int seconds)
 //{
@@ -19,35 +20,91 @@ using System.Reactive.Linq;
 
 //Countdown(5);
 
-HttpRequestQuery httpRequestQuery = new HttpRequestQuery();
+//HttpRequestQuery httpRequestQuery = new HttpRequestQuery();
 
-Observable
-    .FromAsync(() => httpRequestQuery.Execute())
-    .ValidateHttpRequestLenght(80)
-    .Retry(3)
-    .Catch(Observable.Return(new HttpRequestError() { Content = "Cats are cool." }))
-    .Subscribe((httpRequestQuery) =>
-    {
-        Console.WriteLine(httpRequestQuery.Content);
-    });
+//Observable
+//    .FromAsync(() => httpRequestQuery.Execute())
+//    .ValidateHttpRequestLenght(80)
+//    .Retry(3)
+//    .Catch(Observable.Return(new HttpRequestError() { Content = "Cats are cool." }))
+//    .Subscribe((httpRequestQuery) =>
+//    {
+//        Console.WriteLine(httpRequestQuery.Content);
+//    });
 
 
-Console.ReadLine();
+//Console.ReadLine();
 
-static class HttpRequestObservableExtensions
+//static class HttpRequestObservableExtensions
+//{
+//    public static IObservable<HttpRequest> ValidateHttpRequestLenght(this IObservable<HttpRequest> observable, int length)
+//    {
+//        return observable
+//            .Select(httpRequest =>
+//            {
+//                if (httpRequest.Content.Length > length)
+//                {
+//                    return Observable.Throw<HttpRequest>(new Exception("Http Request was too long"));
+//                }
+
+//                return Observable.Return(httpRequest);
+//            })
+//            .Switch();
+//    }
+//};
+
+//Observer pattern RX.NET
+
+using Observable observable = new();
+Observer observer = new();
+observable.Subscribe(observer);
+
+observable.Publish(new("Hello, World!"));
+observable.Publish(new("Goodbye, World!"));
+
+public sealed record Message(string body);
+
+
+public class Observer : IObserver<Message>
 {
-    public static IObservable<HttpRequest> ValidateHttpRequestLenght(this IObservable<HttpRequest> observable, int length)
+    public void OnCompleted()
     {
-        return observable
-            .Select(httpRequest =>
-            {
-                if (httpRequest.Content.Length > length)
-                {
-                    return Observable.Throw<HttpRequest>(new Exception("Http Request was too long"));
-                }
-
-                return Observable.Return(httpRequest);
-            })
-            .Switch();
+        Console.WriteLine($"OnCompleted fired");
     }
-};
+
+    public void OnError(Exception error)
+    {
+
+    }
+
+    public void OnNext(Message value)
+    {
+        Console.WriteLine($"OnNext fired: {value.body}");
+    }
+}
+
+public class Observable : IObservable<Message>, IDisposable
+{
+    private readonly Subject<Message> _subject;
+
+    public Observable()
+    {
+        _subject = new Subject<Message>();
+    }
+
+    public void Dispose()
+    {
+        _subject.OnCompleted();
+        _subject.Dispose();
+    }
+
+    public IDisposable Subscribe(IObserver<Message> observer)
+    {
+        return _subject.Subscribe(observer);  
+    }
+
+    public void Publish(Message value)
+    {
+        _subject.OnNext(value);
+    }
+}
