371dbdfb215e549d1594615f30dc0e4e0b7d64b6
Jack Griffiths Rico <j.griffithsrico@gmail.com>
2026-02-18 13:16:25 +0000

    pay funcion to redirect to stripe for payment

--- FirstMVC/wwwroot/js/site.js
+++ FirstMVC/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function pay(priceId) {
+    fetch('api/stripe/pay', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(priceId)
+    })
+    .then(response => response.text())
+    .then(url => {
+        window.location.href = url;
+    })
+    .catch(error => console.error('Error:', error));
+}
