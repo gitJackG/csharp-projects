97708d54c9f18a4a7edb901eb938d8592b1a5600
Jack Griffiths Rico <j.griffithsrico@gmail.com>
2026-02-18 13:15:21 +0000

    changed stripe product id to string and added stripe price id

--- FirstMVC/Models/Product.cs
+++ FirstMVC/Models/Product.cs
@@ -1,14 +1,20 @@
-﻿namespace FirstMVC.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FirstMVC.Models
 {
     public class Product
     {
+
         public int ProductID { get; set; }
+        public string StripeProductId { get; set; }
+
+        public string StripePriceId { get; set; }
 
         public string ProductName { get; set; }
 
         public string ProductDescription { get; set; }
 
-        public string ProductImage { get; set; }
+        public string? ProductImage { get; set; }
         public int ProductPrice { get; set; }
 
         public Product()
